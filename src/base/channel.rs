4bf6e2c5049ef212e64dcb74cd07750bc02e4817
Sébastien LUNDI <slundi@gmail.com>
2021-05-23 22:25:58 +0200

    Channel information, constants to check

---
+++ src/base/channel.rs
@@ -0,0 +1,80 @@
+use std::collections::HashMap;
+
+const DEFAULT_NAMES: [&'static str; 128] = ["Piano", "Bright Piano", "Electric Grand", "Honky Tonk Piano", "Electric Piano 1", "Electric Piano 2",
+                                            "Harpsichord", "Clavinet", "Celesta",
+                                            "Glockenspiel",
+                                            "Music Box",
+                                            "Vibraphone", "Marimba", "Xylophone", "Tubular Bell",
+                                            "Dulcimer",
+                                            "Hammond Organ", "Perc Organ", "Rock Organ", "Church Organ", "Reed Organ",
+                                            "Accordion",
+                                            "Harmonica",
+                                            "Tango Accordion",
+                                            "Nylon Str Guitar", "Steel String Guitar", "Jazz Electric Gtr", "Clean Guitar", "Muted Guitar", "Overdrive Guitar", "Distortion Guitar", "Guitar Harmonics",
+                                            "Acoustic Bass", "Fingered Bass", "Picked Bass", "Fretless Bass", "Slap Bass 1", "Slap Bass 2", "Syn Bass 1", "Syn Bass 2",
+                                            "Violin", "Viola", "Cello", "Contrabass",
+                                            "Tremolo Strings", "Pizzicato Strings",
+                                            "Orchestral Harp",
+                                            "Timpani",
+                                            "Ensemble Strings", "Slow Strings", "Synth Strings 1", "Synth Strings 2",
+                                            "Choir Aahs", "Voice Oohs", "Syn Choir",
+                                            "Orchestra Hit",
+                                            "Trumpet", "Trombone", "Tuba", "Muted Trumpet", "French Horn", "Brass Ensemble", "Syn Brass 1", "Syn Brass 2",
+                                            "Soprano Sax", "Alto Sax", "Tenor Sax", "Baritone Sax",
+                                            "Oboe", "English Horn", "Bassoon", "Clarinet", "Piccolo", "Flute", "Recorder", "Pan Flute", "Bottle Blow", "Shakuhachi", "Whistle", "Ocarina",
+                                            "Syn Square Wave", "Syn Saw Wave", "Syn Calliope", "Syn Chiff", "Syn Charang", "Syn Voice", "Syn Fifths Saw", "Syn Brass and Lead",
+                                            "Fantasia", "Warm Pad", "Polysynth", "Space Vox", "Bowed Glass", "Metal Pad", "Halo Pad", "Sweep Pad", "Ice Rain", "Soundtrack", "Crystal", "Atmosphere",
+                                            "Brightness", "Goblins", "Echo Drops", "Sci Fi",
+                                            "Sitar", "Banjo", "Shamisen", "Koto", "Kalimba",
+                                            "Bag Pipe",
+                                            "Fiddle",
+                                            "Shanai",
+                                            "Tinkle Bell",
+                                            "Agogo",
+                                            "Steel Drums", "Woodblock", "Taiko Drum", "Melodic Tom", "Syn Drum", "Reverse Cymbal",
+                                            "Guitar Fret Noise", "Breath Noise",
+                                            "Seashore", "Bird", "Telephone", "Helicopter", "Applause", "Gunshot"];
+
+pub struct Channel<'a> {
+    pub channel_id: i32,
+	pub bank: i8,
+	pub program: i8,
+	pub volume: i8,
+	pub balance: i8,
+	pub chorus: i8,
+	pub reverb: i8,
+	pub phaser: i8,
+	pub tremolo: i8,
+	pub name: &'a str,
+    /// Channel parameters (key-value)
+	pub parameters: HashMap<String, String>
+}
+
+/*
+const DEFAULT_PERCUSSION_CHANNEL: i8 = 9;
+const DEFAULT_PERCUSSION_PROGRAM: i8 = 0;
+const DEFAULT_PERCUSSION_BANK: i16 = 128;
+
+const DEFAULT_BANK: i8 = 0;
+const DEFAULT_PROGRAM: i8 = 25;
+const DEFAULT_VOLUME: i8 = 127;
+const DEFAULT_BALANCE: i8 = 64;
+const DEFAULT_CHORUS: i8 = 0;
+const DEFAULT_REVERB: i8 = 0;
+const DEFAULT_PHASER: i8 = 0;
+const DEFAULT_TREMOLO: i8 = 0;*/
+impl Default for Channel<'_> {
+    fn default() -> Self { Channel {
+        channel_id: 1,
+        bank: 0,
+        program: 25,
+        volume: 127,
+        balance: 0,
+        chorus: 0,
+        reverb: 0,
+        phaser: 0,
+        tremolo: 0,
+        name: "UNDEFINED",
+        parameters: HashMap::new()
+    }}
+}
