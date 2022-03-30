87f7c3eea6f9348c111b00a3b2502f928e5f2b02
Sébastien LUNDI <slundi@gmail.com>
2022-03-30 15:19:59 +0200

    Use clap v3

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 extern crate clap;
-use clap::{Arg, App};
+use clap::Parser;
 use std::path::Path;
 use std::ffi::OsStr;
 use std::fs;
@@ -12,25 +12,28 @@ use std::io::Read;
 
 const GUITAR_FILE_MAX_SIZE:usize = 16777216; //16 MB, it should be enough
 
+#[derive(Parser, Debug)]
+#[clap(author="slundi", version="0.1.0", about="Read guitar pro files", long_about = None)]
+struct Args {
+    /// Input file
+    #[clap(short='i', long, help="Input file path")]
+    input: String,
+}
+
 fn main() {
-    //TODO: use clap 3.x
-    let matches = App::new("Guitar IO").version("0.1.0").author("slundi <mail>").about("Read guitar file ")
-    .arg(
-         Arg::with_name("input_file").takes_value(true).required(true).short("i").long("input").value_name("input_file").help("Input file path")
-    ).get_matches();
-    let file = matches.value_of("input_file").unwrap_or("default.conf");
-    let f = Path::new(&file);
+    let args: Args = Args::parse();
+    let f = Path::new(&args.input);
     //check if path OK, file exists and is file
-    if !f.exists() || !f.is_file() {panic!("Unable to access file: {}", &file);}
+    if !f.exists() || !f.is_file() {panic!("Unable to access file: {}", &args.input);}
     //check file format
     let ext = f.extension().and_then(OsStr::to_str).unwrap_or_else(||{panic!("Cannont get input file extension");}).to_uppercase();
-    let size: usize = fs::metadata(file).unwrap_or_else(|e|{panic!("Unable to get file size")}).len() as usize;
+    let size: usize = fs::metadata(&args.input).unwrap_or_else(|_e|{panic!("Unable to get file size")}).len() as usize;
     if size > GUITAR_FILE_MAX_SIZE {panic!("File is too big (bigger than 16 MB)");}
-    let f = fs::OpenOptions::new().read(true).open(file).unwrap_or_else(|_error| {
+    let f = fs::OpenOptions::new().read(true).open(&args.input).unwrap_or_else(|_error| {
         /*if error.kind() == fs::ErrorKind::NotFound {panic!("File {} was not found", &file);}
         else if error.kind() == fs::ErrorKind::PermissionDenieds {panic!("File {} is unreadable, check permissions", &file);}
         else {panic!("Unknown error while opening {}", &file);}*/
-        panic!("Unknown error while opening {}", &file);
+        panic!("Unknown error while opening {}", args.input);
     });
     let mut data: Vec<u8> = Vec::with_capacity(size);
     f.take(u64::from_ne_bytes(size.to_ne_bytes())).read_to_end(&mut data).unwrap_or_else(|_error|{panic!("Unable to read file contents");});
