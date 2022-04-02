4986c4540d7be32dc1b5e920820124f6633f0fca
Sébastien LUNDI <slundi@gmail.com>
2022-04-02 13:59:58 +0200

    Read MixTableChange

--- src/mix_table.rs
+++ src/mix_table.rs
@@ -1,4 +1,7 @@
+use fraction::ToPrimitive;
+
 use crate::rse::*;
+use crate::io::*;
 
 /// A mix table item describes a mix parameter, e.g. volume or reverb
 #[derive(Clone)]
@@ -49,5 +52,64 @@ impl Default for MixTableChange { fn default() -> Self { MixTableChange { instru
 impl MixTableChange {
     pub fn is_just_wah(&self) -> bool {
         return self.instrument.is_none() &&  self.volume.is_none() && self.balance.is_none() && self.chorus.is_none() && self.reverb.is_none() && self.phaser.is_none() && self.tremolo.is_none() && self.tempo.is_none() && self.wah.is_none();
+    }
+    /// Read mix table change. List of values is read first. See `read_values()`.
+    /// 
+    /// List of values is followed by the list of durations for parameters that have changed. See `read_durations()`.
+    pub fn read(data: &Vec<u8>, seek: &mut usize) -> MixTableChange {
+        let mut tc = MixTableChange::default();
+        tc.read_values(data, seek);
+        tc.read_durations(data, seek);
+        return tc;
+    }
+    /// Read mix table change values. Mix table change values consist of 7 `signed-byte` and an `int`, which correspond to:
+    /// - instrument
+    /// - volume 
+    /// - balance
+    /// - chorus
+    /// - reverb
+    /// - phaser
+    /// - tremolo
+    /// - tempo
+    /// 
+    /// If signed byte is *-1* then corresponding parameter hasn't changed.
+    fn read_values(&mut self, data: &Vec<u8>, seek: &mut usize) {
+        //instrument
+        let b = read_signed_byte(data, seek);
+        if b >= 0 {self.instrument = Some(MixTableItem{value: b.to_u8().unwrap(), ..Default::default()});}
+        //volume
+        let b = read_signed_byte(data, seek);
+        if b >= 0 {self.volume = Some(MixTableItem{value: b.to_u8().unwrap(), ..Default::default()});}
+        //balance
+        let b = read_signed_byte(data, seek);
+        if b >= 0 {self.balance = Some(MixTableItem{value: b.to_u8().unwrap(), ..Default::default()});}
+        //chorus
+        let b = read_signed_byte(data, seek);
+        if b >= 0 {self.chorus = Some(MixTableItem{value: b.to_u8().unwrap(), ..Default::default()});}
+        //reverb
+        let b = read_signed_byte(data, seek);
+        if b >= 0 {self.reverb = Some(MixTableItem{value: b.to_u8().unwrap(), ..Default::default()});}
+        //phaser
+        let b = read_signed_byte(data, seek);
+        if b >= 0 {self.phaser = Some(MixTableItem{value: b.to_u8().unwrap(), ..Default::default()});}
+        //tremolo
+        let b = read_signed_byte(data, seek);
+        if b >= 0 {self.tremolo = Some(MixTableItem{value: b.to_u8().unwrap(), ..Default::default()});}
+        //tempo
+        let b = read_signed_byte(data, seek);
+        if b >= 0 {self.tempo = Some(MixTableItem{value: b.to_u8().unwrap(), ..Default::default()});}
+    }
+    /// Read mix table change durations. Durations are read for each non-null `MixTableItem`. Durations are encoded in `signed-byte`.
+    fn read_durations(&mut self, data: &Vec<u8>, seek: &mut usize) {
+        if self.volume.is_some()  {self.volume.take().unwrap().duration  = read_signed_byte(data, seek).to_u8().unwrap();}
+        if self.balance.is_some() {self.balance.take().unwrap().duration = read_signed_byte(data, seek).to_u8().unwrap();}
+        if self.chorus.is_some()  {self.chorus.take().unwrap().duration  = read_signed_byte(data, seek).to_u8().unwrap();}
+        if self.reverb.is_some()  {self.reverb.take().unwrap().duration  = read_signed_byte(data, seek).to_u8().unwrap();}
+        if self.phaser.is_some()  {self.phaser.take().unwrap().duration  = read_signed_byte(data, seek).to_u8().unwrap();}
+        if self.tremolo.is_some() {self.tremolo.take().unwrap().duration = read_signed_byte(data, seek).to_u8().unwrap();}
+        if self.tempo.is_some()   {
+            self.tempo.take().unwrap().duration = read_signed_byte(data, seek).to_u8().unwrap();
+            self.hide_tempo = false;
+        }
     }
 }
